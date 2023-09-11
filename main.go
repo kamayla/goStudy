487a59eb01d1a8efa3befbaa01ef3c6f971457c4
ippei <ippei_kamimura@icloud.com>
2023-09-11 09:33:33 +0900

    add git ignore

--- main.go
+++ main.go
@@ -1,30 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+)
 
-type OrderService interface {
-	Apply(int) error
-}
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
 
-type ServiceImpl struct{}
+	eg, ctx := errgroup.WithContext(ctx)
 
-func (c *ServiceImpl) Apply(id int) error {
-	fmt.Println(id)
-	return nil
-}
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			!errors.Is(err, http.ErrServerClosed) {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
 
-type Application struct {
-	OrderService
-}
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
 
-func (app *Application) Run(id int) error {
-	return app.Apply(id)
+	return eg.Wait()
 }
 
 func main() {
-	app := Application{
-		OrderService: &ServiceImpl{},
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
-
-	_ = app.Run(19)
 }
